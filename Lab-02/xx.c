95abb1dcaec08d3d919dea885db4ccc6bfd1314c
priyanshuiips2026 <priyanshu.iips2026@gmail.com>
2026-09-08 12:24:59 +0530

    Update and rename xx to xx.c

---
+++ Lab-02/xx.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+// program to sum three integers a,b,c
+int main(){
+  int a,b,c,d;
+  printf("Enter a:");
+  printf("Enter b:");
+  printf("Enter c:");
+ scanf("%d %d %d",&a &b &c);
+ d=a+b+c;
+ printf("Total Sum:",d);
+return 0;}
